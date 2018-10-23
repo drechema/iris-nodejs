f29ac709dad82d4e7bbeade762b772dd83763572
David Reche <david.reche@intersystems.com>
2018-10-23 12:52:08 +0200

    Save object, retrieve all objects

--- routes/test.js
+++ routes/test.js
@@ -2,30 +2,55 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/database.js');
 
-// GET /test
-router.get('/', function (req, res) {
+function getNode(id) {
     let node = {
-        global: 'test',
-        subscripts: [1]
+        global: 'test'
     };
-    db.retrieve(node, 'object', (error, result) => {
+    if (id) {
+        node.subscripts = new Array();
+        node.subscripts.push(id);
+    };
+    return node;
+}
+
+// GET /test
+// Get all the entities in 'test' global
+router.get('/', function (req, res) {
+    db.retrieve(getNode(), 'list', (error, result) => {
         if (error) {
             res.status(500).send(result);
         } else {
-            res.status(200).send(result.object);
+            var entities = new Array();
+            result.forEach(element => {
+                let entity = retrieveEntityAt(element);
+                entities.push(entity);
+            });
+            res.status(200).send(entities);
         }
     })
 });
 
+// Retrieve on entity from global
+function retrieveEntityAt(id) {
+    result = db.retrieve(getNode(id), 'object');
+    result.object.id = id;
+    return result.object;
+}
+
 // POST /test
 router.post('/', function (req, res) {
+    // get new id
+    var counter = db.increment(getNode());
+    if (!counter.ok) {
+        res.status(500).send('Cannot get new id');
+        return;
+    }
+    // save object with new id
     let data = {
-        node: {
-            global: 'test',
-            subscripts: [1]
-        },
+        node: getNode(counter.data),
         object: req.body
     };
+    console.log(data);
     db.update(data, 'object', (error, result) => {
         if (error) {
             res.status(500).send();
